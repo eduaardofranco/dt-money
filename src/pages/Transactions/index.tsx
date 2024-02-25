81f28c04329420dae832f709b08909b59870164a
eduaardofranco <eduaardo.franco@gmail.com>
2024-02-25 19:02:36 +0000

    transactions table styles and html

--- src/pages/Transactions/index.tsx
+++ src/pages/Transactions/index.tsx
@@ -1,12 +1,49 @@
 import { Header } from '../../components/Header'
 import { Summary } from '../../components/Summary'
-import { Container } from './styles'
+import { Container, PriceHighlight, TransactionsContainer, TransactionsTable } from './styles'
 
 export function Transactions() {
     return(
         <Container>
             <Header />
             <Summary />
+            <TransactionsContainer>
+                <TransactionsTable>
+                    <tbody>
+                        <tr>
+                            <td width="50%">Burger</td>
+                            <td>
+                                <PriceHighlight variant="outcome">
+                                    - £20.50
+                                </PriceHighlight>
+                            </td>
+                            <td>Food</td>
+                            <td>12/02/2024</td>
+                        </tr>
+                        <tr>
+                            <td width="50%">Wages</td>
+                            <td>
+                                <PriceHighlight variant="income">
+                                     £2.000
+                                </PriceHighlight>
+                            </td>
+                            <td>Salary</td>
+                            <td>10/02/2024</td>
+                        </tr>
+                        <tr>
+                            <td width="50%">Vodafone</td>
+                            <td>
+                                <PriceHighlight variant="outcome">
+                                    - £20
+                                </PriceHighlight>
+                                </td>
+                            <td>Internet</td>
+                            <td>10/02/2024</td>
+                        </tr>
+                    </tbody>
+                </TransactionsTable>
+
+            </TransactionsContainer>
         </Container>
     )
 }
--- src/pages/Transactions/styles.ts
+++ src/pages/Transactions/styles.ts
@@ -3,3 +3,37 @@ import styled from 'styled-components'
 export const Container = styled.div`
 
 `;
+export const TransactionsContainer = styled.div`
+    width: 100%;
+    max-width: 1120px;
+    margin: 6.4rem auto 0;
+    padding: 0 2.4rem;
+`;
+
+export const TransactionsTable = styled.table`
+    border-collapse: separate;
+    border-spacing: 0 .8rem;
+    /* margin-top: 2.4rem; */
+    width: 100%;
+    td {
+        padding: 2rem 3.2rem;
+        background: ${props => props.theme['gray-700']};
+
+        &:first-child {
+            border-top-left-radius: 6px;
+            border-bottom-left-radius: 6px;
+        }
+
+        &:last-child {
+            border-top-right-radius: 6px;
+            border-bottom-right-radius: 6px;
+        }
+    }
+`;
+interface PriceHighlightProps {
+    variant: 'income' | 'outcome'
+}
+export const PriceHighlight = styled.span<PriceHighlightProps>`
+
+    color: ${props => props.variant === 'income' ? props.theme['green-300'] : props.theme['red-300']};
+`;
