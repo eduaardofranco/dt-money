5d86b8e12d6117e7f6100983b1f616d395a4ae33
eduaardofranco <eduaardo.franco@gmail.com>
2024-02-26 12:44:00 +0000

    radio button transaction modal

--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -1,6 +1,6 @@
 import * as Dialog from '@radix-ui/react-dialog'
-import { CloseButton, Content, Overlay } from './styles'
-import { X } from 'phosphor-react'
+import { CloseButton, Content, Overlay, TransactionType, TransactionTypeButton } from './styles'
+import { ArrowCircleDown, ArrowCircleUp, X } from 'phosphor-react'
 
 export function NewTransactionModal() {
     return(
@@ -20,6 +20,16 @@ export function NewTransactionModal() {
                     <input type='text' placeholder='Description' required />
                     <input type='number' placeholder='Price' required />
                     <input type='text' placeholder='Category' required />
+                    <TransactionType>
+                        <TransactionTypeButton variant="income" value="income">
+                            <ArrowCircleUp size={32} />
+                                Income
+                        </TransactionTypeButton>
+                        <TransactionTypeButton variant="outcome" value="outcome">
+                            <ArrowCircleDown size={32} />
+                                Outcome
+                        </TransactionTypeButton>
+                    </TransactionType>
 
                     <button type="submit">Register</button>
                 </form>
